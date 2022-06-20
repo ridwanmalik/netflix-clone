3a74784a8e448b930ef38287c01e035bcd344a05
ridwanmalik <skredonemalik@gmail.com>
2022-06-20 23:54:11 +0600

    Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,16 @@ module.exports = {
         'gray-web': '#808080',
       },
       height: {
+        '10vh': '10vh',
+        '20vh': '20vh',
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh',
+        '60vh': '60vh',
+        '70vh': '70vh',
         '75vh': '75vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
         '95vh': '95vh',
       },
       backgroundImage: {
